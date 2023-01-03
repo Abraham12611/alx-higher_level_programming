8b357e888257297949f0f929309d731c14b9f746
Abraham12611 <abraham.dahunsi@gmail.com>
2023-01-03 07:26:06 -0800

    Fixed bug

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-str1, str2 = "Holberton", "School"
-message = f"Welcome to {str1} " \
-          f"{str2}!"
-print(message)
+str1 = "Holberton"
+str2 = "School"
+str1 = str1 + " " + str2
+print(f"Welcome to {str1}!")
 

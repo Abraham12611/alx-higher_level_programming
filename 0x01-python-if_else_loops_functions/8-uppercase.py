bfbc1e7bb986b4613ec7a8a8a5c63972a21d2060
Abraham12611 <abraham.dahunsi@gmail.com>
2023-01-08 08:13:42 -0800

    Print uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        if ord('a') <= ord(str[i] <= ord('z'):
+                letter = 32
+                else
+                letter = 0
+                print('{:c}'.format(ord(str[i]) - letter))

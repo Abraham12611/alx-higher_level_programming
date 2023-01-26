3ad62ee9d524d04158b02e44dac4ba86993ac530
Abraham12611 <abraham.dahunsi@gmail.com>
2023-01-26 09:42:29 -0800

    Square element in matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    if len(matrix) == 0:
+        return new_matrix
+
+    new_matrix = [[i*i for i in col]for row in matrix]
+    return new_matrix

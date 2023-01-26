8e208e12e58e7ef3f8df67d8bcf3b0d92a4635a7
Abraham12611 <abraham.dahunsi@gmail.com>
2023-01-26 10:01:49 -0800

    Replace for Search

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,5 +4,5 @@ def square_matrix_simple(matrix=[]):
     if len(matrix) == 0:
         return new_matrix
 
-    new_matrix = [[i*i for i in col]for row in matrix]
+    new_matrix = [[i*i for i in col] for row in matrix]
     return new_matrix

4d89bc845643f668269a8e540b16bd386cf46a0c
Abraham12611 <abraham.dahunsi@gmail.com>
2023-01-27 08:06:10 -0800

    Print squared values

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    list_keys = list(a_dictionary.keys())
-    list_keys.sort()
+    new_dir = a_dictionary.copy()
+    list_keys = list(new_dir.keys())
+
     for i in list_keys:
-        print("{}: {}".format(i, a_dictionary.get(i) ** 2))
+        new_dir[i] *= 2
+
+    return (new_dir)

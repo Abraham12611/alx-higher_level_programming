2b5d0dde86291abb22f9a9ae258d71f84d9b5117
Abraham12611 <abraham.dahunsi@gmail.com>
2023-02-21 00:39:33 -0800

    Task complete

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    import importlib
-    def print_module_names(file_path):
-        hidden_4 = importlib.import_module("hidden_4")
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
 
-        names = dir(hidden_4)
-
-        for name in names:
-            if not name.startswith("__"):
-                print(name)
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
